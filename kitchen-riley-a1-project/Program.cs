534ee664437d75ecdfaec0f0b543d27ee9dd59a2
rKitchen223 <riley.kitchen@mohawkcollege.ca>
2024-09-23 14:54:56 -0400

    Program Update

---
+++ kitchen-riley-a1-project/Program.cs
@@ -0,0 +1,53 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.ComponentModel.Design;
+
+Console.WriteLine("What is your name?")
+
+string playerName = (Console.ReadLine());
+
+Console.WriteLine($"Hello {playerName}, welcome to my town");
+Console.WriteLine("these are my people. We are on a journey to find our missing diamond tha has been taken by an evil dark magic villain")
+
+  Console.WriteLine("Hi bobbby the blacksmith, what are my 4 options for weapons I can pick from the slot")
+Console.WriteLine("Diamond Sword, Dragon Sword, Dark Blade Sword, Green Light Sword");
+string playerChoice = Console.ReadLine();
+
+if (playerChoice == "Diamond Sword")
+{
+    Console.WriteLine("You Buy a Diamond Sword")
+}
+
+else if (playerChoice == "Dragon Sword")
+{
+    Console.WriteLine("You buy a Dragon Sword")
+}
+
+if (playerChoice == "Dark Blade Sword")
+{
+    Console.WriteLine("You Buy a Dark Blade Sword")
+}
+
+else if (playerChoice == "Green Light Sword")
+{
+    Console.WriteLine("You Buy a Green Light Sword")
+}
+Console.WriteLine("You bought a Diamond Sword")
+
+
+
+
+
+
+//Player Start, Provide Options to travel
+
+
+//Option #1
+
+
+//Option #2
+
+
+//Option #3
+
+
