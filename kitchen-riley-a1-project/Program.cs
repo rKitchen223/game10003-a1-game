94d2d99d92d44fe8f68b194c442f41ff043e8721
rKitchen223 <riley.kitchen@mohawkcollege.ca>
2024-09-26 00:50:10 -0400

    Game Program

--- kitchen-riley-a1-project/Program.cs
+++ kitchen-riley-a1-project/Program.cs
@@ -1,38 +1,129 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 using System.ComponentModel.Design;
+using System.Runtime.InteropServices;
 
-Console.WriteLine("What is your name?")
+Console.WriteLine("Hello, Welcome to my World, Please tell me your name so we can start our game");
 
-string playerName = (Console.ReadLine());
+string playerName = Console.ReadLine();
 
-Console.WriteLine($"Hello {playerName}, welcome to my town");
-Console.WriteLine("these are my people. We are on a journey to find our missing diamond tha has been taken by an evil dark magic villain")
+Console.WriteLine($"Hello {playerName}, it is very nice to meet you");
+Console.WriteLine("These are my people in the town and this is where your journey would start for your first mission. We are on a journey to find our missing diamond stone that has been taken by an evil dark magic villian named lethal");
 
-  Console.WriteLine("Hi bobbby the blacksmith, what are my 4 options for weapons I can pick from the slot")
+Console.WriteLine("The first person you will be meeting is bobby the blacksmith, he is the person that you will be meeting when you want any type of weapons from him.");
+Console.WriteLine("To start with your first weapon, what weapon would you want to choose to start your adventure");
 Console.WriteLine("Diamond Sword, Dragon Sword, Dark Blade Sword, Green Light Sword");
 string playerChoice = Console.ReadLine();
 
 if (playerChoice == "Diamond Sword")
 {
-    Console.WriteLine("You Buy a Diamond Sword")
+    Console.WriteLine("You Buy a Diamond Sword");
 }
 
-else if (playerChoice == "Dragon Sword")
+if (playerChoice == "Dragon Sword") ;
 {
-    Console.WriteLine("You buy a Dragon Sword")
+    Console.WriteLine("You buy a Dragon Sword");
 }
 
-if (playerChoice == "Dark Blade Sword")
+if (playerChoice == "Dark Blade Sword");
 {
-    Console.WriteLine("You Buy a Dark Blade Sword")
-}
 
-else if (playerChoice == "Green Light Sword")
-{
-    Console.WriteLine("You Buy a Green Light Sword")
 }
-Console.WriteLine("You bought a Diamond Sword")
+Console.WriteLine("You Buy a Dark Blade Sword");
+
+ if (playerChoice == "Green Light Sword");
+{
+   Console.WriteLine("You Buy a Green Light Sword");
+
+ Console.WriteLine("if no weapon is pick, unvaild Choice.");
+    string (playerChoice = Console.ReadLine();
+    Console.WriteLine("&d", playerChoice);
+
+    Console.WriteLine("When you picked out your sword that you bought from the blacksmith name bobby, you go on a quest to find one of the stone pieces in a dark Castle island");
+    Console.WriteLine("When you find the island, you go to explore it and you leave your things behind");
+    Console.WriteLine("When you explore the island you see an enemie that is guarding one of the pieces for the stone");
+    Console.WriteLine("To defeat the enemie you have to choose which 4 abilities to use on the enemie with the weapon you have");
+    Console.WriteLine("Lighting Strike, Dragon Force, Dark Enegry Power, Nature Force Power");
+    if (playerChoice == "Lighting Strike") ;
+
+    Console.WriteLine("You choose the Lighting Strike ability");
+
+    if (playerChoice == "Dragon Force") ;
+
+    Console.WriteLine("You choose the Dragon Force ability");
+
+    if (playerChoice == "Dark Enegry Power") ;
+
+    Console.WriteLine("You choose the Dark Enegry Power ability");
+
+    if (playerChoice == "Nature Force Power") ;
+
+    Console.WriteLine("You choose the Nature Force Power");
+
+    Console.WriteLine("If no weapon is pick, unvaid Choice.");
+
+    Console.WriteLine("With the ability that you choose you manage to defeat the enemy and you are granted with a rare stone.");
+    Console.WriteLine("This stone will allow you to contiune on your journey to find more of the other stones.");
+    Console.WriteLine("Now you began your search for other islands or places to explore.");
+    Console.WriteLine("You will be given the opporunities to find treasures beyond your dreams, but upon seeking theses riches you will encounter obstacles that will decide weather you will survive another quests or die and restart from the beginning");
+    Console.WriteLine("Fnding these treasures, you will be rewarded powers beyond your dreams");
+    Console.WriteLine("You will be given 3 obstacles to choose, only one will lead to glory and two will lead you to defeat");
+    Console. WriteLine("1. mythical creatures, 2. horrific weathers, 3. strategically placed barriers");
+
+    if (playerChoice == "mythical creatures)";
+
+    Console.WriteLine("When you choose the mythical creatures pathway, you are met by alot of different types of creatures that have different abilities but the stone is not in that pathway.");
+
+    if (playerChoice == "horrific weathers") ;
+
+    Console.WriteLine("When you choose the horrific weathers pathway, you are met with hurricanes, flooding, tornadoes, blizzards, sandstorms, and extreame heats. Your going into that path that has an environmental danger and you will need more than just weapons to get through. The stone is not there!");
+
+    if (playerChoice == "strategically placed barriers") ;
+
+    Console.WriteLine("When you choose the barriers pathway, you will be searching for a gold key and this key will open the door to a treasure chest that will hold a mythical stone piece");
+
+    Console.WriteLine("If you don't choose any of the pathways, you are unable to procede any furthur");
+
+    Console.WriteLine("If you choose the right pathway you will be rewarded with the mythical stone");
+    Console.WriteLine("When you retrieve the rare stone and the mythical stone, you gain lots of power and strength when combining the two stone");
+    Console.WriteLine("You will have unmatchable powers to defeat the dark magic villian named lethal)";
+    Console.WriteLine("For your final mission you set out to destroy and conquer the dark magic villian");
+    Console.WriteLine("When you find the evil magic villian castle, you go to explore inside the castle");
+    Console.WriteLine("Inside the castle you are met with your last challenge to retreive the diamond stone");
+    Console.WriteLine("You look around the castle and you see lethal is wearing the diamond stone around his neck");
+    Console.WriteLine("This will be the final battle, the battle is gonna take place in the castle.");
+    Console.WriteLine("You have 5 options to use against lethal and 1 option will lead to victory");
+    Console.WriteLine("Lighting Strike with the Diamond Sword, Dragon Force with the Dragon Sword, Dark Enegry Power with the Dark Blade Sword, Nature Force Power with the Green Light Sword, and Super powers when he combines the stone together");
+
+    if (playerChoice == "Lighting Strike with Diamond Sword") ;
+
+    Console.WriteLine("The abilities won't be good if he is resistant against lighting attacks, but the Diamond Sword would be good to use");
+
+    if (playerChoice == "Dragon Force with Dragon Sword") ;
+
+    Console.WriteLine("The abilities will be good if he is not resistant against Dragon attacks, but with the Dragon Sword he won't be good to use it");
+
+    if (playerChoice == "Dark Enegry Power with Dark Blade Sword") ;
+
+    Console.WriteLine("You can't use the abilities for Dark powers if he has Dark powers, you also can't use the Dark Blade Sword since it is a Dark power sword");
+
+    if (playerChoice == "Nature Force Power with Green Light Sword") ;
+
+    Console.WriteLine("You can use the Nature Force Power because it is a very powerful attack, but you can't use the Green Light Sword since it is not very effective");
+
+    if (playerChoice == "Superpowers when combine stones together") ;
+
+    Console.WriteLine("You can use the Superpower stones to be able to defeat lethal with a very powerful attack");
+
+    Console.WriteLine("Once you pick your option to use against lethal, he gets defeated and the whole castle starts to crumble down with his magic being erase");
+    Console.WriteLine("When he is defeated you see the diamond stone on the ground and you go to pick it up and bring it back to where it belonges in the town city");
+    Console.WriteLine("Once you bring it back, the people in the town say thank you for helping us retrieve the diamond stone");
+    Console.WriteLine("You say that you are welcome and you become the new leader of the town city protecting the diamond stone");
+    Console.WriteLine("The End!");
+
+
+
+
 
 
 
